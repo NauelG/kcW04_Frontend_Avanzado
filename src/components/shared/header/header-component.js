ede0c98bdc147cdf85e68b54d9dd8acc3740e0db
NauelG <g.nauel@gmail.com>
2018-09-22 19:38:41 +0200

    Header component funcionando

--- src/components/shared/header/header-component.js
+++ src/components/shared/header/header-component.js
@@ -3,14 +3,31 @@ import './header-component.scss';
 
 export const Header = new Component('app-header');
 
-const data = Header.getData();
-
 Header.template = `
-    <header>
-        Hola mundo desde el header modificado
-        <p>${data[0].text}</p>
+    <header class="header">
+        <h1 class="logo">Think<span>G</span>eek</h1>
+        <nav class="menu">
+                <a href="#">Tecnología</a>
+                <a href="#">Programación</a>
+                <a href="#">Redes y Seguridad </a>
+                <a href="#">Big Data</a>
+                <a id="toggle-menu" class="toggle-menu" href="#">
+                    <i class="fa fa-bars"></i> 
+                </a>
+        </nav>
     </header>
 `;
 
+Header.toggleMenu = () => {
+    const menu = document.querySelector('nav.menu');
+    const button = document.querySelector('a#toggle-menu');
+    button.addEventListener('click', () => {
+        console.log('click')
+        menu.classList.toggle('menu-open');
+    })
+};
+
+
 Header.render();
+Header.toggleMenu();
 export default Header;
