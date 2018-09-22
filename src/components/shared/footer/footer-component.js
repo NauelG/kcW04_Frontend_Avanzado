50dea93e4e92f4dd897d689e7ba7e45382f4bd0e
NauelG <g.nauel@gmail.com>
2018-09-22 20:20:14 +0200

    Footer hecho

--- src/components/shared/footer/footer-component.js
+++ src/components/shared/footer/footer-component.js
@@ -4,8 +4,13 @@ import './footer-component.scss'
 const Footer = new Component('app-footer');
 
 Footer.template = `
-    <footer>
-        Todos los derechos reservados
+    <footer class="footer">
+        <nav class="footer-icons">
+            <a href="https://www.linkedin.com/in/nauelg" target="_blank" title="linkedIn"><i class="fab fa-linkedin"></i></a>
+            <a href="https://twitter.com/GNauel" target="_blank" title="Twitter"><i class="fab fa-twitter-square"></i></a>
+            <a href="https://github.com/NauelG/" target="_blank" title="GitHub"><i class="fab fa-github-square"></i></a>
+            </nav>
+        <span class="footer-copy">Todos los derechos reservados © 2018 | Página creada por <strong>Nauel G.</strong></span>
     </footer>
 `;
 
