b63d765e9bd792cfae17e67db65c26899480b3f9
NauelG <g.nauel@gmail.com>
2018-09-23 00:11:28 +0200

    Home component casi acabado

--- src/components/home/home-component.js
+++ src/components/home/home-component.js
@@ -1,12 +1,47 @@
 import { Component } from 'services/Component';
+import { ArticleService } from 'services/articles-service';
 import './home-component.scss';
 
 export const Home = new Component('app-home');
 
-Home.template = `
-    <section>
-        Home component - Render
-    </section>
-`;
+const _articlesSrv = new ArticleService();
+
+let articles = [];
+_articlesSrv.getArticles().then(
+    response => {
+        createTemplate(response)
+    }
+);
+
+const createTemplate = (items) => {
+        console.log(items)
+        Home.template = `
+        <section class="container songs">
+        ${items.map(item => `
+        
+            <div class="card">
+                <h2 class="card-title">${item.title}</h2>
+                <img class="card-image" src="${item.picture}" alt="${item.title}">
+                <p class="card-content">
+                    ${item.intro}
+                </p>
+                <div class="card-footer">
+                    <img class="card-profile-image" src="${item.authorImage}" alt="${item.author}">
+                    <span class="card-author">${item.author}</span>
+                    <span class="card-date">${resolveDate(item.date)}</span>
+                    <a href="./article.html?id=${item.id}" class="btn card-btn">Ver comentarios - <span class="card-count">${item.comments.length}</span></a>
+                </div>
+            </div>
+
+      `).join('')}
+        </section>
+    `;
+    Home.render();
+}
+
+const resolveDate = (dateString) => {
+    return dateString;
+}
+
 
 export default Home;
